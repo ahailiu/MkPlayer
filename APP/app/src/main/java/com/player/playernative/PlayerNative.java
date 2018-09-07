5d6e1d646addc79e97adb6ff1c08b74575381cfd
ahailiu <343253928@qq.com>
2018-09-07 23:30:30 +0800

    added native interface

--- APP/app/src/main/java/com/player/playernative/PlayerNative.java
+++ APP/app/src/main/java/com/player/playernative/PlayerNative.java
@@ -1,6 +1,10 @@
 package com.player.playernative;
 
+import android.net.Uri;
 import android.util.Log;
+import android.view.Surface;
+
+import java.util.Map;
 
 /**
  * Created by ahailiu-MC1 on 2018/9/7.
@@ -22,4 +26,42 @@ public class PlayerNative {
     private long mNativeContext;
 
     private static native final void native_init();
+
+    public PlayerNative() {
+
+    }
+
+    public void setSurface(Surface surface) {
+
+    }
+
+    public void setDataSource(Uri uri) {
+
+    }
+
+    public void setDataSource(Uri uri, Map<String, String> headers) {
+
+    }
+
+    public void setParameter(String[] keys, String[] values) {
+
+    }
+
+    public native void prepareAsync();
+
+    public void prepared() {
+
+    }
+
+    public void start() {
+
+    }
+
+    public void stop() {
+
+    }
+
+    public void pause() {
+
+    }
 }
